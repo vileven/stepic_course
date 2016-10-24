fdcd0dc51b29208abe10b2a147b487304a11526a
vileven <vileven11@icloud.com>
2016-10-25 00:59:27 +0300

    second for stepic

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn conf
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask_volodin
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 
