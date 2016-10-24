30112bf2a0bd360dd4e99d633e845a3afab6ecce
vileven <vileven11@icloud.com>
2016-10-24 08:59:20 +0300

    ds

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo rm -r /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
