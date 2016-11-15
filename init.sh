15d734bf1e84636dfcc8d85b6e723bb291d85dee
vileven <vileven11@icloud.com>
2016-11-15 08:25:34 +0300

    third task begin

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn conf
+sudo rm -r /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
